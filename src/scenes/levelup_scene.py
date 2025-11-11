2445801734369b2d358e03d1f847db82ad17120b
joinjun001 <109087027+Joinjun001@users.noreply.github.com>
2025-11-11 14:30:22 +0900

    SCRUM-62 : github-jira test

---
+++ src/scenes/levelup_scene.py
@@ -0,0 +1,6 @@
+import pygame
+
+
+class LevelUpScene:
+    def __init__(self):
+        pass
--- src/systems/exp_manager.py
+++ src/systems/exp_manager.py
@@ -1,13 +1,28 @@
-
+from src.core.game_state import GameState
+# from src.scenes.levelup_scene import LevelupScene
 
 class ExpManager:
+    """
+       Handles player EXP increase, level-up detection, and level-up logic.
 
-    def is_level_up(self):
-        # 1)
-        pass
+       Methods:
+           exp_up(amount): called when an enemy dies to increase EXP.
+           is_level_up(): checks if EXP exceeds required level EXP.
+           level_up(): triggers the level-up process (UI + stat increase).
+           apply_level_choice(choice): applies selected bonus to GameState.
+    """
+
+    def __init__(self, game_state: GameState, scene_manager):
+        self.game_state = game_state
+        self.scene_manager = scene_manager
 
     def exp_up(self):
         pass
+
+    def is_level_up(self):
+        pass
+
+
 
     def level_up(self):
         pass
